3d145b741ece8b49a4a611484cd9bc30001c4b5d
Eugene Vedensky <Work@Genyas-MacBook-Air.local>
2017-06-01 23:38:59 -0400

    proof of concept needs html cleaned up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { BodyComponent } from './body/body.component';
+import { SectionComponent } from './section/section.component';
+import { PublicationListComponent } from './publication-list/publication-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    BodyComponent,
+    SectionComponent,
+    PublicationListComponent
   ],
   imports: [
     BrowserModule,
